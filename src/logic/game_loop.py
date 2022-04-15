a8b7ed12e7275501c0fa9521fe102bfc207d38f7
Teemu Koski <80845642+TemeKoo@users.noreply.github.com>
2022-04-15 14:21:50 +0300

    Siirrettiin pelin pyöritys GameLooppiin

--- src/logic/game_loop.py
+++ src/logic/game_loop.py
@@ -1,7 +1,11 @@
 import pygame
-from logic.event_handler import EventHandler
-from render.renderers import Renderer
+from game_objects.field import Field
+from game_objects.tower import Tower
 from helpers.level_loader import LevelLoader
+from render.renderers import Renderer
+from ui.buttons import TowerButton
+
+from logic.event_handler import EventHandler
 
 
 class GameLoop():
@@ -9,6 +13,7 @@ class GameLoop():
         self.clock = pygame.time.Clock()
         self.fps = 30
         self.running = False
+
         self.event_handler = EventHandler()
         self.renderer = Renderer(screen)
         self.loader = LevelLoader()
@@ -17,17 +22,23 @@ class GameLoop():
             "scene": "level_1",
             "scene_type": "level",
             "sprites": pygame.sprite.Group(),
-            "placing_tower": None,
-            "tower_placement_valid": False
+            "towers": pygame.sprite.Group(),
+            "field": Field(1, self.loader)
         }
+
+        new_tower = Tower(300, 220)
+        self.scene_data["towers"].add(new_tower)
+        self.scene_data["sprites"].add(new_tower)
+        self.scene_data["sprites"].add(TowerButton(40, 400))
 
     def loop(self):
         while self.running:
-            self.running = self.event_handler.handle_events(self.scene_data["sprites"])
+            self.running = self.event_handler.handle_events(self.scene_data)
             self.renderer.render(self.scene_data)
             self.clock.tick(self.fps)
 
     def run(self):
         if not self.running:
+            self.renderer.set_scene(self.scene_data)
             self.running = True
             self.loop()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame
 
+from logic.game_loop import GameLoop
 from game_objects.field import Field
 from game_objects.tower import Tower
 from helpers.level_loader import LevelLoader
@@ -10,48 +11,12 @@ from ui.buttons import TowerButton
 
 def main():
     pygame.init()
-
     pygame.display.set_caption("game test")
 
     screen: pygame.Surface = pygame.display.set_mode((640, 480))
+    game_loop = GameLoop(screen)
+    game_loop.run()
 
-    loader = LevelLoader()
-
-    field = Field(1, loader)
-
-    tower = Tower(300, 220)
-
-    towers = pygame.sprite.Group()
-    towers.add(tower)
-
-    all_sprites = pygame.sprite.Group()
-
-    for twer in towers:
-        all_sprites.add(twer)
-
-    button = TowerButton(40, 400)
-    all_sprites.add(button)
-
-    event_handler = EventHandler()
-    renderer = Renderer(screen)
-
-    scene_data = {
-        "sprites": all_sprites,
-        "towers": towers,
-        "scene": "level_1",
-        "scene_type": "level",
-        "field": field
-    }
-
-    renderer.set_scene(scene_data)
-
-    clock = pygame.time.Clock()
-
-    running = True
-    while running:
-        running = event_handler.handle_events(scene_data)
-        renderer.render(scene_data)
-        clock.tick(30)
 
 if __name__ == "__main__":
     main()
