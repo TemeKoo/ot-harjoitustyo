2997ccd1d0a91c568ee86c2b520e9662d51b56b4
Teemu Koski <80845642+TemeKoo@users.noreply.github.com>
2022-04-24 02:11:50 +0300

    Aaltojen lisäys dataan

--- src/helpers/level_loader.py
+++ src/helpers/level_loader.py
@@ -11,9 +11,14 @@ class LevelLoader():
 
     def _check_level(self, level: int) -> str:
         level = str(level)
-        if level in self._levels_dict:
-            return level
-        return None
+        return level if level in self._levels_dict else None
+
+    def _check_wave(self, level: int, wave: int) -> str:
+        level = self._check_level(level)
+        wave = str(wave)
+        if level is not None and wave in self._levels_dict[level]["waves"]:
+            return level, wave
+        return None, None
 
     def get_name(self, level: int) -> str:
         level = self._check_level(level)
@@ -23,14 +28,20 @@ class LevelLoader():
 
     def get_field(self, level: int) -> list:
         level = self._check_level(level)
-        if level:
+        if level is not None:
             return self._levels_dict[level]["field"]
         return None
 
     def get_path(self, level: int) -> list:
         level = self._check_level(level)
-        if level:
+        if level is not None:
             path = self._levels_dict[level]["path"]
             path = [(i[0], i[1]) for i in path]
             return path
+        return None
+
+    def get_wave(self, level: int, wave: int) -> dict:
+        level, wave = self._check_wave(level, wave)
+        if level is not None:
+            return self._levels_dict[level]["waves"][wave]
         return None
